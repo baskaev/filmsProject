cb1f51a41677ee01e3d353ddafd83e155b2adfd5
Taimuraz Baskaev <osnovatahmuraz@gmail.com>
2024-12-11 23:07:33 +0300

    main

---
+++ gitGoMain.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Папка, в которой находятся ваши проекты
+base_dir=""
+
+# Перебираем все подпапки в base_dir
+for dir in "$base_dir"/*; do
+  # Проверяем, что это директория
+  if [ -d "$dir" ]; then
+    echo "Заходим в директорию: $dir"
+    cd "$dir" || continue
+    # Проверяем, является ли директория репозиторием git
+    if [ -d ".git" ]; then
+      echo "Выполняем git checkout main в $dir"
+      git checkout main
+    else
+      echo "Не git репозиторий в $dir"
+    fi
+    cd "$base_dir" || continue
+  fi
+done
